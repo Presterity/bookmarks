07486da4a16d91a77832be3838742d78265142d6
Joanna Power <joanna@cozi.com>
2017-01-30 22:05:09 -0800

    Bookmark schema

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# bookmarks
+# Bookmarks project
 Datastore and APIs for bookmarked reference material.
+
+## Development
+
+### Requirements
+
+* git: https://git-scm.com/downloads
+* Python 3: https://www.python.org/downloads/
+* virtualenv: http://docs.python-guide.org/en/latest/dev/virtualenvs/
+* Postgres: https://www.postgresql.org/download/
+
+### Getting Started
+
+1. Install [git](https://git-scm.com/downloads), [Python 3](https://www.python.org/downloads/), and [virtualenv](http://docs.python-guide.org/en/latest/dev/virtualenvs/).
+
+1. Get set up to [connect to github with ssh](https://help.github.com/articles/connecting-to-github-with-ssh/)
+
+1. Create virtual environment and clone `bookmarks` repo:
+
+  ```bash
+  cd ~/projects
+  virtualenv --python python3 --prompt presterity- venvs/presterity
+  source venvs/presterity/bin/activate
+  git clone git@github.com:presterity/bookmarks
+  pip install -r requirements.txt
+  ```
+
+
+### Set up your database
+
+1. Install [Postgres](https://www.postgresql.org/download/) ([Ubuntu 14.04](https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-14-04))
+
+1. Create super user for yourself:
+
+  ```bash
+  sudo -i -u postgres
+  createuser --interactive
+  Enter name of role to add: <username>
+  Shall the new role be a superuser? (y/n) y
+  createdb <username>
+  ```
+
+1. Create `presterity` schema and user/role:
+
+  ```sql
+  psql
+  postgres=# create schema presterity;
+  postgres=# create user presterity password <password>;
+  postgres=# grant all on schema presterity to presterity;
+  ```
+
+4. Connect to `presterity` database as `presterity` user and create schema:
+
+  ```sql
+  psql -U presterity -d presterity -h 127.0.0.1 -W
+  presterity => \conninfo
+  You are connected to database "presterity" as user "presterity" on host "127.0.0.1" at port "5432".
+  SSL connection (cipher: DHE-RSA-AES256-GCM-SHA384, bits: 256)
+  presterity => \i sql/bookmark-schema-001.sql
+  CREATE FUNCTION
+  psql:sql/bookmark-schema-001.sql:14: NOTICE:  table "bookmark" does not exist, skipping
+  DROP TABLE
+  CREATE TABLE
+  CREATE TRIGGER
+  psql:sql/bookmark-schema-001.sql:58: NOTICE:  table "bookmark_topic" does not exist, skipping
+  DROP TABLE
+  CREATE TABLE
+  CREATE INDEX
+  CREATE TRIGGER
+  psql:sql/bookmark-schema-001.sql:78: NOTICE:  table "bookmark_note" does not exist, skipping
+  DROP TABLE
+  CREATE TABLE
+  CREATE INDEX
+  CREATE TRIGGER
+  presterity => \dt
+                List of relations
+   Schema |      Name      | Type  |   Owner    
+  --------+----------------+-------+------------
+   public | bookmark       | table | presterity
+   public | bookmark_note  | table | presterity
+   public | bookmark_topic | table | presterity
+  (3 rows)
+  ```
---
+++ sql/bookmark-schema-001.sql
@@ -0,0 +1,98 @@
+/* Core bookmark schema */
+
+-- Stored procedure for setting updated column
+CREATE OR REPLACE FUNCTION set_audit_updated()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.audit_updated = now();
+    RETURN NEW;	
+END;
+$$ language 'plpgsql';
+
+
+-- Main bookmark table
+DROP TABLE IF EXISTS bookmark CASCADE;
+CREATE TABLE bookmark (
+
+  -- bookmark_id is UUID stored as 8-4-4-4-12
+  bookmark_id CHAR(36) NOT NULL PRIMARY KEY,
+
+  -- url of referenced article
+  url VARCHAR(2000) NOT NULL,
+
+  -- user-supplied summary of article
+  summary TEXT NOT NULL,
+
+  -- datetime of event that is topic of article
+  sort_date_utc TIMESTAMPTZ NOT NULL,
+  
+  -- status of bookmark; e.g. 'not relevant', 'duplicate', 'accepted', etc.
+  status VARCHAR(100) DEFAULT NULL,
+
+  -- partial or full date that will be displayed with bookmark: YYYY, YYYY.mm, YYYY.mm.dd, YYYY.mm.dd HH, YYYY.mm.dd HH:MM
+  display_date VARCHAR(50) NOT NULL,
+
+  -- source of bookmark, e.g. 'raindrop'
+  source VARCHAR(50) DEFAULT NULL,
+
+  -- identifier of bookmark by source, e.g. raindrop_id 
+  source_bookmark_id VARCHAR(100) DEFAULT NULL,
+  
+  -- last_updated date on source; used to decide if bookmark needs to be refreshed in data store
+  source_last_updated TIMESTAMPTZ DEFAULT NULL,
+
+  -- id of person who submitted bookmark; might be email, Twitter handle, or something else
+  submitter_id VARCHAR(100) DEFAULT NULL,
+  
+  -- date on which bookmark was submitted
+  submission_date_utc TIMESTAMPTZ DEFAULT NULL,
+
+  -- for ops only
+  audit_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  audit_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+CREATE TRIGGER BOOKMARK_AUDIT_UPDATED BEFORE UPDATE ON bookmark FOR EACH ROW EXECUTE PROCEDURE set_audit_updated();
+
+
+-- Topics associated with bookmark; bookmark will appear in timeline on these topic pages
+DROP TABLE IF EXISTS bookmark_topic CASCADE;
+CREATE TABLE bookmark_topic (
+  bookmark_id CHAR(36) NOT NULL,
+  topic VARCHAR(100) NOT NULL,
+
+  -- date available for business logic
+  created_on TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+  -- for ops only
+  audit_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  audit_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+  PRIMARY KEY (bookmark_id, topic),
+  FOREIGN KEY (bookmark_id) REFERENCES bookmark
+);
+CREATE INDEX BOOKMARK_TOPIC_TOPIC_IDX ON bookmark_topic USING btree (topic);
+CREATE TRIGGER BOOKMARK_TOPIC_AUDIT_UPDATED BEFORE UPDATE ON bookmark_topic FOR EACH ROW EXECUTE PROCEDURE set_audit_updated();
+
+
+-- Volunteer or application-specified notes on bookmark; i.e. "duplicate of bookmark id 567"
+DROP TABLE IF EXISTS bookmark_note CASCADE;
+CREATE TABLE bookmark_note (
+  note_id CHAR(36) NOT NULL PRIMARY KEY,
+  bookmark_id CHAR(36) NOT NULL,
+  note TEXT NOT NULL,
+
+  -- id of voluteer or application who authored note
+  author VARCHAR(100) NOT NULL,
+
+  -- date available for business logic
+  created_on TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+  -- For ops only
+  audit_created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  audit_updated TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+  FOREIGN KEY (bookmark_id) REFERENCES bookmark
+);
+CREATE INDEX BOOKMARK_NOTE_BOOKMARK_ID_IDX ON bookmark_note USING btree (bookmark_id);
+CREATE TRIGGER BOOKMARK_NOTE_AUDIT_UPDATED BEFORE UPDATE ON bookmark_note FOR EACH ROW EXECUTE PROCEDURE set_audit_updated();
+
