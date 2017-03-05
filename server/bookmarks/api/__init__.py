d4f86dd144d6d87cca015ca9e84dcf8e2e87d8f4
Joanna Power <joanna@cozi.com>
2017-03-05 10:26:25 -0800

    First tests for API endpoints

--- server/bookmarks/api/__init__.py
+++ server/bookmarks/api/__init__.py
@@ -1,14 +1,23 @@
 """REST API for the bookmark manager.
 """
 
+import logging
 from typing import Optional, Any, Callable
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 import bookmarks.dao as dao
+from .response_formatter import ResponseFormatter
 
-# Create Flask app
+
+# Create Flask app and set up logging
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+logger = app.logger
+
+
+# End request hook
+#dao.Session.close(commit=True)
 
 
 # API methods / endpoints
@@ -21,13 +30,17 @@ def info():
     """Return structured information about the running application."""
     return jsonify({'nothing_to_see': 'not yet'})
 
-
 @app.route("/api/1702/bookmark/", methods=['GET'])
 def get_bookmarks():
     """Retrieve bookmarks, optionally filtered by topic.
+
+    Optional topics are read from query string: topic=...&topic=...
     """
-    response_data = [ResponseFormatter.format_bookmark(b, version=1702) 
-                     for b in dao.Bookmark.select_bookmarks()]
+    topics = None
+    if 'topic' in request.args:
+        topics = request.args.getlist('topic')
+    bookmarks = dao.Bookmark.select_bookmarks(topics=topics)
+    response_data = ResponseFormatter.format_bookmarks_response(bookmarks, version=1702) 
     return jsonify(response_data), 200
 
 @app.route("/api/1702/bookmark/<string:bookmark_id>", methods=['GET'])
@@ -39,6 +52,6 @@ def get_bookmark_by_id(bookmark_id):
         response_data = {'error': 'bookmark not found'}
         status_code = 404
     else:
-        response_data = ResponseFormatter.format_bookmark(bookmark)
+        response_data = ResponseFormatter.format_bookmark(bookmark, version=1702)
         status_code = 200
     return jsonify(response_data), 200 
--- server/bookmarks/api/test_endpoints.py
+++ server/bookmarks/api/test_endpoints.py
@@ -2,21 +2,21 @@
 """
 
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import ANY, Mock, patch
 import uuid
 
 import json
 
-import bookmarks.dao as dao
+import bookmarks.dao
 from bookmarks.dao import TestDaoFactory
-from bookmarks.api import app
+import bookmarks.api
 
 
 class BookmarkManagerApiTests(unittest.TestCase):
     """Verify REST endpoints.
     """
     def setUp(self):
-        self.app = app.test_client()
+        self.app = bookmarks.api.app.test_client()
         self.app.testing = True 
 
     def get_bookmarks(self, topics=None):
@@ -24,22 +24,48 @@ class BookmarkManagerApiTests(unittest.TestCase):
         """
         uri = '/api/1702/bookmark/'
         if topics:
-            uri += '?' + '&'.join(topics)
+            uri += '?' + '&'.join(['topic={0}'.format(t) for t in topics])
         return self.app.get(uri)
 
-    @patch.object(dao.Bookmark, 'select_bookmarks')
-    def test_get_bookmarks(self, mock_select_bookmarks):
+    @patch.object(bookmarks.api.ResponseFormatter, 'format_bookmarks_response')
+    @patch.object(bookmarks.dao.Bookmark, 'select_bookmarks')
+    def test_get_bookmarks(self, mock_select_bookmarks, mock_format_response):
         """Verify success scenario for retrieving bookmarks with no topics.
         """
         # Set up mocks and test data
         test_bookmarks = [TestDaoFactory.create_bookmark(), TestDaoFactory.create_bookmark()]
         mock_select_bookmarks.return_value = test_bookmarks
+        mock_format_response.return_value = mock_response_json = {'test_key': 'test_value'}
         
         # Make call
         response = self.get_bookmarks()
         
-        print("\nRESPONSE")
-        print(response.data)
+        # Verify response
+        self.assertEqual(mock_response_json, json.loads(response.data.decode(encoding='utf-8')))
+
+        # Verify mocks
+        mock_select_bookmarks.assert_called_once_with(topics=None)
+        mock_format_response.assert_called_once_with(test_bookmarks, version=ANY)
+
+    @patch.object(bookmarks.api.ResponseFormatter, 'format_bookmarks_response')
+    @patch.object(bookmarks.dao.Bookmark, 'select_bookmarks')
+    def test_get_bookmarks__topics(self, mock_select_bookmarks, mock_format_response):
+        """Verify success scenario for retrieving bookmarks with topics.
+        """
+        # Set up mocks and test data
+        test_bookmarks = [TestDaoFactory.create_bookmark(), TestDaoFactory.create_bookmark()]
+        mock_select_bookmarks.return_value = test_bookmarks
+        mock_format_response.return_value = mock_response_json = {'test_key': 'test_value'}
+        
+        # Make call
+        response = self.get_bookmarks(topics=['topic_1', 'topic_2'])
+        
+        # Verify response
+        self.assertEqual(mock_response_json, json.loads(response.data.decode(encoding='utf-8')))
+
+        # Verify mocks
+        mock_select_bookmarks.assert_called_once_with(topics=['topic_1', 'topic_2'])
+        mock_format_response.assert_called_once_with(test_bookmarks, version=ANY)
 
 
 
