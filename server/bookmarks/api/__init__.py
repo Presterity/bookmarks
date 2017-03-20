3af72564b860a22d209f909da7b7138028b6494d
Julie Pitt <julie@yakticus.com>
2017-03-19 17:21:20 -0700

    work in progress: parsing display date sent by client, beginnings of add new bookmark request

--- server/bookmarks/api/__init__.py
+++ server/bookmarks/api/__init__.py
@@ -8,11 +8,15 @@ import logging
 from typing import Optional, Any, Callable
 
 from flask import Flask, jsonify, request
+from flask_api import status
+from werkzeug.exceptions import BadRequest
 
 import bookmarks.dao as dao
 from .path_converters import AnyIntConverter
 from .response_formatter import ResponseFormatter
+from .date_parser import  DateParser, DateParseError
 
+import bookmarks.dao as dao
 
 # Create Flask app and set up basic logging
 app = Flask(__name__)
@@ -27,8 +31,10 @@ app.url_map.converters['any_int'] = AnyIntConverter
 def before_first_request():
     dao.Session.initialize()
 
+
 def before_request():
     dao.Session.get()
+
 
 def after_request(response):
     dao.Session.close(commit=True)
@@ -40,10 +46,12 @@ def after_request(response):
 def root():
     return 'OK'
 
+
 @app.route('/info')
 def info():
     """Return structured information about the running application."""
     return jsonify({'nothing_to_see': 'not yet'})
+
 
 @app.route("/api/<any_int(1702):version>/bookmark/", methods=['GET'])
 def get_bookmarks(version=None):
@@ -56,7 +64,64 @@ def get_bookmarks(version=None):
         topics = request.args.getlist('topic')
     bookmarks = dao.Bookmark.select_bookmarks(topics=topics)
     response_data = ResponseFormatter.format_bookmarks_response(bookmarks, version=version) 
-    return jsonify(response_data), 200
+    return jsonify(response_data), status.HTTP_200_OK
+
+
+@app.route("/api/<any_int(1702):version>/bookmark/", methods=['POST'])
+def post_bookmarks(version=None):
+    """Create a new bookmark andd assign it a new UUID.
+
+    Expected request body:
+    {
+      "summary"          : <string>
+      "display_date"     : <string: YYYY, YYYY.mm, YYYY.mm.dd, YYYY.mm.dd HH, YYYY.mm.dd HH:MM>
+      "url"              : <string>
+      "description" [OPT]: <string>
+      "topics"      [OPT]: [<string>, ...]
+    }
+    """
+    # TODO much of this logic can be shared by the PUT method (put_bookmarks)
+
+    # returns None if mimetype is not application/json. Throws BadRequest exception on malformed JSON.
+    bookmark_json = request.get_json()
+    if bookmark_json:
+        bookmark = dao.Bookmark()
+        try:
+            bookmark.summary = getattr(bookmark_json, 'summary')
+            bookmark.url = getattr(bookmark_json, 'url')
+            display_date = getattr(bookmark_json, 'display_date')
+        except AttributeError:
+            # issue 400 response when an expected attribute is missing
+            # TODO include message specific to the failure?
+            raise BadRequest()
+
+        try:
+            # parse display date to get sort_date and display_date_format
+            sort_date, date_format = DateParser.parse_date(display_date)
+            bookmark.sort_date = sort_date
+            bookmark.display_date_format = date_format
+        except DateParseError:
+            # issue 400 response display date can't be parsed
+            # TODO include message specific to the failure?
+            raise BadRequest()
+
+        if hasattr(bookmark_json, 'description'):
+            bookmark.description = getattr(bookmark_json, 'description')
+
+        if hasattr(bookmark_json, 'topics'):
+            # TODO set up topics object
+            pass
+
+        # TODO persist bookmark and/or topics here
+
+        status_code = status.HTTP_200_OK
+        response_data = ResponseFormatter.format_bookmark(bookmark, version=version)
+    else:
+        response_data = {}
+        status_code = status.HTTP_404_NOT_FOUND
+
+    return jsonify(response_data), status_code
+
 
 @app.route("/api/<any_int(1702):version>/bookmark/<string:bookmark_id>", methods=['GET'])
 def get_bookmark_by_id(bookmark_id, version=None):
@@ -65,10 +130,10 @@ def get_bookmark_by_id(bookmark_id, version=None):
     bookmark = dao.Bookmark.select_bookmark_by_id(bookmark_id)
     if not bookmark:
         response_json = ''
-        status_code = 404
+        status_code = status.HTTP_404_NOT_FOUND
     else:
         response_json = jsonify(ResponseFormatter.format_bookmark(bookmark, version=version))
-        status_code = 200
+        status_code = status.HTTP_200_OK
     return response_json, status_code
 
 
---
+++ server/bookmarks/api/test_date_parser.py
@@ -0,0 +1,34 @@
+"""Tests for DateParser
+"""
+
+import datetime
+import unittest
+
+from bookmarks.api.date_parser import DateParser, DateParseError
+
+
+class DateParserTests(unittest.TestCase):
+
+    def assert_success(self, date_str: str, expected_format: str, expected_date: datetime.datetime):
+        date, date_format = DateParser.parse_date(date_str)
+        self.assertEqual(date_format, expected_format)
+        self.assertEqual(date, expected_date)
+
+    def test_parse_date__Y(self):
+        self.assert_success('1975', '%Y', datetime.datetime(1975, 1, 1))
+
+    def test_parse_date__YM(self):
+        self.assert_success('1985.03', '%Y.%m', datetime.datetime(1985, 3, 1))
+
+    def test_parse_date__YMD(self):
+        self.assert_success('1993.01.21', '%Y.%m.%d', datetime.datetime(1993, 1, 21))
+
+    def test_parse_date__YMD_H(self):
+        self.assert_success('2001.12.23 07', '%Y.%m.%d %H', datetime.datetime(2001, 12, 23, 7))
+
+    def test_parse_date__YMD_HM(self):
+        self.assert_success('2017.03.23 06:28', '%Y.%m.%d %H:%M', datetime.datetime(2017, 3, 23, 6, 28))
+
+    def test_parse_date_invalid(self):
+        with self.assertRaises(DateParseError):
+            DateParser.parse_date('blarg')
